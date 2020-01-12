6802a597f39a56c8b163621ddb99c5e061be521c
Magda Czyz <stajniaenklawa@gmail.com>
2020-01-12 15:37:30 +0100

    zadanie 13

---
+++ src/main/java/com/sda/services/Calculator.java
@@ -0,0 +1,15 @@
+package com.sda.services;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Calculator {
+    @Setter
+    @Getter
+    private int n;
+
+    public int square() {
+        return n*n;
+    }
+}
